e8995e12108553cc5259bd01e63f5a123e118724
learnwithcolt <learnwithcolt@gmail.com>
2016-02-03 04:49:25 +0000

    update dburl

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-// mongoose.connect("mongodb://localhost/yelp_camp_v10");
-mongoose.connect("mongodb://colt:rusty@ds055525.mongolab.com:55525/yelpcamp");
+
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://colt:rusty@ds055525.mongolab.com:55525/yelpcamp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
